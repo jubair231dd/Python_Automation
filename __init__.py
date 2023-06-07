d15ae1f43260c74d84f04d5255fa884b106fa78b
jubair231dd <jubairpantho1@gmail.com>
2023-06-07 14:13:25 -0400

    Edited the text

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ directory be regcognized as a module by the python
 
 #!/usr/bin/env python3
 This command says that the command for running the .py file is python3
+
 Instead of calling python3 areas.py 
-it just need ./areas.py to call the file
+it just need ./areas.py to call the file.
 
 
-for importing a module
+For importing a module:
+
 1. import python3
 2. import "module name"
 3. module.functioname(arg)
