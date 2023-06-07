566de9d9c9e0a7f4b4c7c2702cb3cbd38a993b51
jubair231dd <jubairpantho1@gmail.com>
2023-06-07 15:44:12 -0400

    Added a script for checking the health of the system

---
+++ health_check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free/du.total*100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+if check_disk_usage("/") and check_cpu_usage():
+    print("Everything is fine")
+else:
+    print("Error! System is using more! ")
